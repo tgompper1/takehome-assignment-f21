eb83834f9e666ba498afc5b6552cd17254ecfbb9
Tess Gompper <tgomp9@gmail.com>
2021-09-05 12:05:15 -0400

    part 3

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,6 +1,13 @@
 <template>
-  <p>I'm a Counter!</p>
+  <div>
+    <p>Current count: {{count}}</p> 
+    <button v-on:click="count+=1">increment</button>
+    <button v-on:click="count-=1" v-if="count>0" >decrement</button> 
+  </div>  
 </template>
+
+//dont want a negative count so us if. as of now I only know how to make the whole button appear or dissapear based on Count.
+
 
 <script>
 export default {
