27060b9ae7adab50d902ce0e5f7bd58867f597c2
Tess Gompper <tgomp9@gmail.com>
2021-09-05 12:45:57 -0400

    part 4

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    <p>Current count: {{count}}</p> 
-    <button v-on:click="count+=1">increment</button>
-    <button v-on:click="count-=1" v-if="count>0" >decrement</button> 
+    <p>Current count: {{initial_count}}</p> 
+    <button v-on:click="initial_count+=1">increment</button>
+    <button v-on:click="initial_count-=1" v-if="initial_count>0" >decrement</button> 
   </div>  
 </template>
 
 //dont want a negative count so us if. as of now I only know how to make the whole button appear or dissapear based on Count.
-
+//not sure how to set count as initial count and then update from there, so i replaced count with initial count
 
 <script>
 export default {
   data() {
     return {
-      count: 0
+      initial_count
     };
+  },
+  props: {
+    initial_count: Number
   }
 };
 </script>
--- vue-frontend/src/components/Show.vue
+++ vue-frontend/src/components/Show.vue
@@ -1,11 +1,26 @@
 <template>
   <!-- PART 4: Add your code here -->
-  <div></div>
+  <div>
+    {{name}}
+    <Counter v-bind:initial_count="episodes_seen"></Counter>
+  </div>
 </template>
 
 <script>
 // PART 4: Add your code here
-export default {};
+
+import Counter from "./Counter.vue";
+
+export default {
+  props:{
+    id: Number,
+    name: String,
+    episodes_seen: Number
+  },
+  components:{
+    Counter
+  }
+};
 </script>
 
 <style>
